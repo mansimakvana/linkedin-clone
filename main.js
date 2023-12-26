a88c359e666df3d3750472855b57f22b42f58344
mansimakvana <mansimakvana2@gmail.com>
2023-12-26 18:08:36 +0530

    final commit

--- main.js
+++ main.js
@@ -3,3 +3,20 @@ let profileMenu = document.getElementById("profile-menu")
 function toggleMenu(){
     profileMenu.classList.toggle("open-menu")
 }
+
+let sidebarActivity = document.getElementById("sidebarActivity")
+let moreLink = document.getElementById("show-more-link")
+
+
+function toggleActivity() {
+    sidebarActivity.classList.toggle("open-activity")
+
+    if(sidebarActivity.classList.contains("open-activity")){
+        moreLink.innerHTML = "Show less <b> - </b>";
+    }
+    else{
+        moreLink.innerHTML = "Show more <b> + </b>";
+    }
+
+}
+
